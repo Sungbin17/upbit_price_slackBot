b20cbedeadb7dbfe6604aacf356dce9a08639c3d
Sungbin17 <wd1kr1@gmail.com>
2018-05-14 17:38:13 +0900

    changed channel to daily ether

--- env.py
+++ env.py
@@ -1,2 +1,5 @@
 Upbit_ETH_daily_url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/days/?code=CRIX.UPBIT.KRW-ETH&count=10'
 
+SLACK_BOT_TOKEN="xoxb-283968633152-362863541508-kuXJL0Et2A6bgek0dVfrWnuL"
+
+bot_test_channel="CALN1QQ3A"
--- upbit_price.py
+++ upbit_price.py
@@ -1,27 +1,46 @@
 import urllib.request, json
-from env import Upbit_ETH_daily_url
+from env import Upbit_ETH_daily_url, SLACK_BOT_TOKEN, bot_test_channel
 from urllib.request import Request, urlopen
+import time
+import datetime
+import schedule
+from slackclient import SlackClient
 
-response = Request(Upbit_ETH_daily_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-data = json.loads(urlopen(response).read())
+def send_price_message():
+	slack_token = SLACK_BOT_TOKEN
 
-yesterday_ETH_price = data[1]
+	response = Request(Upbit_ETH_daily_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-date = yesterday_ETH_price.get('candleDateTime')[:10]
+	data = json.loads(urlopen(response).read())
 
-highPrice = yesterday_ETH_price.get('highPrice')
+	sc = SlackClient(slack_token)
 
-lowPrice = yesterday_ETH_price.get('lowPrice')
+	yesterday_ETH_price = data[1]
 
-averagePrice = (highPrice + lowPrice) / 2
+	date = yesterday_ETH_price.get('candleDateTime')[:10]
 
-won = 10000000 / averagePrice
+	highPrice = yesterday_ETH_price.get('highPrice')
 
-won = round(won, 3)
+	lowPrice = yesterday_ETH_price.get('lowPrice')
 
-print(date, highPrice, lowPrice, averagePrice)
+	averagePrice = (highPrice + lowPrice) / 2
+
+	won = 10000000 / averagePrice
+
+	won = round(won, 3)
+
+	show_price = str(date) + ' 1ETH = ' + str(averagePrice) + ' 1천만원당 ' + str(won) + 'ETH' + ' 기준 - 저:' + str(lowPrice) + ' 고: ' + str(highPrice) + ' (업비트)' 
+
+	print(show_price)
+
+	sc.api_call("chat.postMessage", channel=bot_test_channel, text=show_price)
+
+
+
+
+schedule.every().day.at("10:00").do(send_price_message)
 
 while True:
-	print(str(date) + ' 1ETH = ' + str(averagePrice) + ' 1천만원당 ' + str(won) + 'ETH' + ' 기준 - 저:' + str(lowPrice) + ' 고: ' + str(highPrice) , '(업비트)' )
-	time.sleep(1000)
+	schedule.run_pending()
+
