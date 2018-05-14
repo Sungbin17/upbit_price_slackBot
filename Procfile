445efb2cbc7b39dd0995d9e916b143b13cc20039
Sungbin17 <wd1kr1@gmail.com>
2018-05-14 13:17:49 +0900

    while true

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python upbit_price.py
--- upbit_price.py
+++ upbit_price.py
@@ -1,8 +1,27 @@
 import urllib.request, json
 from env import Upbit_ETH_daily_url
+from urllib.request import Request, urlopen
 
-response = urllib.request.urlopen(Upbit_ETH_daily_url)
+response = Request(Upbit_ETH_daily_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-data = json.loads(response.read())
+data = json.loads(urlopen(response).read())
 
-print(data[0])
+yesterday_ETH_price = data[1]
+
+date = yesterday_ETH_price.get('candleDateTime')[:10]
+
+highPrice = yesterday_ETH_price.get('highPrice')
+
+lowPrice = yesterday_ETH_price.get('lowPrice')
+
+averagePrice = (highPrice + lowPrice) / 2
+
+won = 10000000 / averagePrice
+
+won = round(won, 3)
+
+print(date, highPrice, lowPrice, averagePrice)
+
+while True:
+	print(str(date) + ' 1ETH = ' + str(averagePrice) + ' 1천만원당 ' + str(won) + 'ETH' + ' 기준 - 저:' + str(lowPrice) + ' 고: ' + str(highPrice) , '(업비트)' )
+	time.sleep(1000)
