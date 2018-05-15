6e71e971cdf909b05f4b7035bd60525511591d65
Sungbin17 <wd1kr1@gmail.com>
2018-05-15 13:04:39 +0900

    erase unnecessary lines
    erase unnecessary lines

--- upbit_price.py
+++ upbit_price.py
@@ -9,36 +9,21 @@ from slackclient import SlackClient
 
 def send_price_message():
 	slack_token = SLACK_BOT_TOKEN
-
 	response = Request(Upbit_ETH_daily_url, headers={'User-Agent': 'Mozilla/5.0'})
-
 	data = json.loads(urlopen(response).read())
-
 	sc = SlackClient(slack_token)
-
 	yesterday_ETH_price = data[1]
-
 	date = yesterday_ETH_price.get('candleDateTime')[:10]
-
 	highPrice = yesterday_ETH_price.get('highPrice')
-
 	lowPrice = yesterday_ETH_price.get('lowPrice')
-
 	averagePrice = (highPrice + lowPrice) / 2
-
 	won = 10000000 / averagePrice
-
 	won = round(won, 3)
-
 	show_price = str(date) + ' 1ETH = ' + str(averagePrice) + ' 1천만원당 ' + str(won) + 'ETH' + ' 기준 - 저:' + str(lowPrice) + ' 고: ' + str(highPrice) + ' (업비트)' 
-
 	print(show_price)
-
 	sc.api_call("chat.postMessage", channel=bot_test_channel, text=show_price)
 
-
-
-
+	
 schedule.every().day.at("10:00").do(send_price_message)
 
 while True:
